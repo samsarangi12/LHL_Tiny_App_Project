32fa85ef933d06d714a6fb61e8a592e5df56a657
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-09 17:33:00 +0000

    Code including express

---
+++ express_server.js
@@ -0,0 +1,23 @@
+
+//Load the packages that we need
+const http = require("http");
+const express = require("express");
+const app = express();
+const PORT = "8080";
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.send(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app is listening on port ${PORT}`)
+});
