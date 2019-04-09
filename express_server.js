8c5d8c870501dc29b92057a6867c3996d394dbdf
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-09 23:42:26 +0000

    Commit after changes for post and update

--- express_server.js
+++ express_server.js
@@ -2,35 +2,61 @@
 //Load the packages that we need
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = "8080";
 
 //setting up view engine as ejs
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateShortURL() {
+  let possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let newShortURL = '';
+  for (let i = 0; i >= 6; i++) {
+    newShortURL += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+
+  return newShortURL
+}
+
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-//test code to display "Hello World!"
+//test route to display "Hello World!"
 app.get("/hello", (req, res) => {
-  let templateVars = { greeting: "Hello World!"}
+  let templateVars = { greeting: "Hello World!"};
   res.render("hello_world", templateVars);
 });
 
-//This code renders the urls page and also provides a link to the create a Tiny URL.
+//This route renders the urls page and also provides a link to the create a Tiny URL.
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase}
+  let templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
   //res.json(urlDatabase);
 });
 
-//This code renders the tiny URLS page.
-app.get("/urls/:shortURL",(req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]}
-  res.render("urls_show", templateVars)
+
+//This route reders the url submission form.
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  //console.log(req.body);
+  res.send("Ok");
 })
+
+//This route renders the tiny URLS page.
+app.get("/urls/:shortURL",(req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app is listening on port ${PORT}`)
