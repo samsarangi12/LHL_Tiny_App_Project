c7e637944d7751c12559b9e425e66407e5e76ae5
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-09 21:37:58 +0000

    Commit after adding urls_index.ejs and urls_show.ejs renders

--- express_server.js
+++ express_server.js
@@ -13,17 +13,24 @@ let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//test code to display "Hello World!"
 app.get("/hello", (req, res) => {
   let templateVars = { greeting: "Hello World!"}
   res.render("hello_world", templateVars);
 });
 
+//This code renders the urls page and also provides a link to the create a Tiny URL.
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase}
   res.render("urls_index", templateVars);
   //res.json(urlDatabase);
 });
+
+//This code renders the tiny URLS page.
+app.get("/urls/:shortURL",(req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]}
+  res.render("urls_show", templateVars)
+})
 
 app.listen(PORT, () => {
   console.log(`Example app is listening on port ${PORT}`)
