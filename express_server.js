8067b1c0b6054b4ab76d5e3011d1b5479cf09e88
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-10 14:13:24 +0000

    Modified some comments

--- express_server.js
+++ express_server.js
@@ -11,6 +11,7 @@ const PORT = "8080";
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//This function creates a random string which will be used for creating the shorturl
 function generateShortURL() {
   let possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let newShortURL = '';
