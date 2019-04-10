a692816564efeb1b530cdfa7381a8060f45bd674
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-10 23:53:56 +0000

    Code changes for login/lgout functionality

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
-//added this line to test git branching
 //Load the packages that we need
 const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = "8080";
 
@@ -10,7 +10,7 @@ const PORT = "8080";
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser())
 //This function creates a random string which will be used for creating the shorturl
 function generateShortURL() {
   let possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -27,24 +27,33 @@ let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-//test route to display "Hello World!"
-app.get("/hello", (req, res) => {
-  let templateVars = { greeting: "Hello World!"};
-  res.render("hello_world", templateVars);
+
+//Route to post the login form
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  console.log(req.body.username);
 });
 
 //This route renders the urls page and also provides a link to the create a Tiny URL.
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase};
+  let templateVars = {username: req.cookies["username"], urls: urlDatabase};
   res.render("urls_index", templateVars);
-  //res.json(urlDatabase);
 });
 
 //This route reders the url submission form.
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
+//This route creates the new tiny url.
 app.post("/urls", (req, res) => {
   let newShortURL = generateShortURL();
   urlDatabase[newShortURL] = req.body.longURL;
@@ -63,18 +72,20 @@ app.post("/urls/:url/delete", (req, res) => {
 app.post("/urls/:id", (req, res) => {
   let shortURL = req.params.id
   let longURL = urlDatabase.shortURL
-  console.log(shortURL)
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect("/urls");
   
 })
 
-//This route renders the tiny URLS page.
+//This route renders the tiny URLS page. 
+
 app.get("/urls/:shortURL",(req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
+  let templateVars = {username: req.cookies["username"], shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
+//This route renders the tiny URLS page. Removing this code since the 
+//functionality is no more required for the app.
 app.get("/u/:shortURL",(req, res) => {
   let templateVars = urlDatabase[req.params.shortURL];
   res.redirect(templateVars)
