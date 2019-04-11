c5fb2588b75c7f14d6fcd7dd68c4e6ebbf32415f
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-11 21:36:01 +0000

    Code added to logout from the app

--- express_server.js
+++ express_server.js
@@ -10,7 +10,7 @@ const PORT = "8080";
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser())
+app.use(cookieParser());
 
 //This function creates a random string which will be used for creating the shorturl
 function generateShortURL() {
@@ -19,7 +19,7 @@ function generateShortURL() {
   for (let i = 0; i < 6; i++) {
     newShortURL += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
   }
-  return newShortURL
+  return newShortURL;
 }
 
 const users = { 
@@ -33,7 +33,7 @@ const users = {
     email: "user2@example.com", 
     password: "dishwasher-funk"
   }
-}
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -46,16 +46,25 @@ function emailLookup(email) {
   let output = false;
   for (element in users) {
     let userlist = users[element];
-    //console.log(userlist)
-    // console.log(users[element].email);
     if(userlist.email === email) {
       output = true;
     }
   }
-  //console.log("display the users", users)
   return output;
 }
 
+//Password lookup function to check if the email entered 
+//by the user is available in the database.
+function passwordLookup(password) {
+  let output = false;
+  for (element in users) {
+    let userlist = users[element];
+    if(userlist.password === password) {
+      output = true;
+    }
+  }
+  return output;
+}
 //Funtion to get email id to pass as the cookie
 function getEmail(userid) {
   let useremail = '';
@@ -76,51 +85,68 @@ app.get("/register", (req, res) => {
 //Route to post the registration
 app.post("/register", (req, res) => {
   if (req.body.email && req.body.password) {
-    let emailPresent = emailLookup(req.body.email)
+    let emailPresent = emailLookup(req.body.email);
     if (emailPresent) {
-      res.status(400).send("Email already present. Please use a new email to register")
+      res.status(400).send("Email already present. Please use a new email to register");
     } else {
       const userid = generateShortURL();
       const useremail = req.body.email;
       const userpwd = req.body.password;
       users[userid] = {id: userid, email:useremail, password: userpwd};
-      //console.log(users);
       res.cookie("userid", userid);
       res.redirect("/urls");
     }
-
   } else {
-    res.status(400).send("Email or password cannot be empty")
+    res.status(400).send("Email or password cannot be empty");
   }
 });
 
-//Route to post the login form
+//Route to get the login form
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//Route to post login
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
+  if (req.body.email && req.body.password) {
+    let emailPresent = emailLookup(req.body.email);
+    let passwordMatch = passwordLookup(req.body.password);
+    if (emailPresent && passwordMatch) {
+      const userid = generateShortURL();
+      const useremail = req.body.email;
+      const userpwd = req.body.password;
+      users[userid] = {id: userid, email:useremail, password: userpwd};
+      res.cookie("userid", userid);
+      res.redirect("/urls");
+    } else if (emailPresent && !passwordMatch) {
+      res.status(403).send("Incorrect Password")
+    } else if (!emailPresent){
+      res.status(403).send("User not registered. Please register");
+    }
+  } else {
+    res.status(400).send("Email or password cannot be empty");
+  }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect("/urls");
+  res.clearCookie('userid');
+  res.redirect("/login");
 });
 
 //This route renders the urls page and also provides a link to the create a Tiny URL.
 app.get("/urls", (req, res) => {
-  let userid = req.cookies["userid"]
+  let userid = req.cookies["userid"];
   let useremail = getEmail(userid);
   let templateVars = {username: useremail, urls: urlDatabase};
-  //let templateVars = {username: users, userid:req.cookies["userid"], urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
 //This route reders the url submission form.
 app.get("/urls/new", (req, res) => {
-  let userid = req.cookies["userid"]
+  let userid = req.cookies["userid"];
   let useremail = getEmail(userid);
   let templateVars = {
     username: useremail
-    //username: users, userid:req.cookies["userid"]
   };
   res.render("urls_new", templateVars);
 });
@@ -134,16 +160,16 @@ app.post("/urls", (req, res) => {
 
 //Route to delete a URL
 app.post("/urls/:url/delete", (req, res) => {
-  let shortURL = req.params.url
-  delete urlDatabase[shortURL]
+  let shortURL = req.params.url;
+  delete urlDatabase[shortURL];
   res.redirect("/urls");
   
 })
 
 //Route to update a URL
 app.post("/urls/:id", (req, res) => {
-  let shortURL = req.params.id
-  let longURL = urlDatabase.shortURL
+  let shortURL = req.params.id;
+  let longURL = urlDatabase.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect("/urls");
   
@@ -152,12 +178,12 @@ app.post("/urls/:id", (req, res) => {
 //This route renders the tiny URLS page. 
 
 app.get("/urls/:shortURL",(req, res) => {
-  let userid = req.cookies["userid"]
+  let userid = req.cookies["userid"];
   let useremail = getEmail(userid);
   let templateVars = {username: useremail, shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app is listening on port ${PORT}`)
+  console.log(`Example app is listening on port ${PORT}`);
 });
