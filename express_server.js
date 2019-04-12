74677cbc40e9b90dfe9efd4909504cf706972d13
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-12 22:15:27 +0000

    Code changes to switch to Encrypted Cookies

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const app = express();
 const PORT = "8080";
@@ -12,6 +13,11 @@ const PORT = "8080";
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(cookieSession({
+  name: "session",
+  keys: ["This is a key for a basic tinyURL APP"],
+  maxAge: 1 * 60 * 1000
+}))
 
 //This function creates a random string which will be used for creating the shorturl
 function generateShortURL() {
@@ -23,6 +29,7 @@ function generateShortURL() {
   return newShortURL;
 }
 
+//UserDatabase
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -36,6 +43,7 @@ const users = {
   }
 };
 
+//URL Database
 const urlDatabase = {
   "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
   "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" },
@@ -68,7 +76,8 @@ function passwordLookup(password) {
   }
   return output;
 }
-//Funtion to get email id to pass as the cookie
+
+//Funtion to get email id to pass as the key
 function getEmail(userid) {
   let useremail = '';
   for (element in users) {
@@ -97,7 +106,7 @@ app.post("/register", (req, res) => {
       const userpwd = req.body.password;
       const hashedPassword = bcrypt.hashSync(userpwd, 10);
       users[userid] = {id: userid, email:useremail, hashedPassword: hashedPassword};
-      res.cookie("userid", userid);
+      req.session.userid = userid;
       res.redirect("/urls");
     }
   } else {
@@ -123,7 +132,7 @@ app.post("/login", (req, res) => {
           userid = userlist.id;
         }
       }
-      res.cookie("userid", userid);
+      req.session.userid = userid;
       res.redirect("/urls");
     } else if (emailPresent && !passwordMatch) {
       res.status(403).send("Incorrect Password")
@@ -135,14 +144,15 @@ app.post("/login", (req, res) => {
   }
 });
 
+//This Route to post's the logout functionality
 app.post("/logout", (req, res) => {
-  res.clearCookie('userid');
+  req.session = null;
   res.redirect("/login");
 });
 
 //This route renders the urls page and also provides a link to the create a Tiny URL.
 app.get("/urls", (req, res) => {
-  let userid = req.cookies["userid"];
+  let userid = req.session.userid;
   let useremail = getEmail(userid);
   let selectedUrlDatabase = {}
   if(userid) {
@@ -160,7 +170,7 @@ app.get("/urls", (req, res) => {
 
 //This route renders the url submission form.
 app.get("/urls/new", (req, res) => {
-  let userid = req.cookies["userid"];
+  let userid = req.session.userid;
   let useremail = getEmail(userid);
   let templateVars = {
     username: useremail
@@ -175,7 +185,7 @@ app.get("/urls/new", (req, res) => {
 
 //This route creates the new tiny url.
 app.post("/urls/new", (req, res) => {
-  let userid = req.cookies["userid"];
+  let userid = req.session.userid;
   let newShortURL = generateShortURL();
   urlDatabase[newShortURL] = { longURL: req.body.longURL, userID: userid};
   res.redirect("/urls");
@@ -183,7 +193,7 @@ app.post("/urls/new", (req, res) => {
 
 //Route to delete a URL
 app.post("/urls/:url/delete", (req, res) => {
-  let userid = req.cookies["userid"];
+  let userid = req.session.userid;
   let shortURL = req.params.url;
   if (userid === urlDatabase[shortURL].userID) {
     delete urlDatabase[shortURL];
@@ -197,7 +207,7 @@ app.post("/urls/:url/delete", (req, res) => {
 //long url associated with a short url
 
 app.get("/urls/:shortURL",(req, res) => {
-  let userid = req.cookies["userid"];
+  let userid = req.session.userid;
   let useremail = getEmail(userid);
   if(userid && urlDatabase[req.params.shortURL].userID === userid) {
     let templateVars = {username: useremail,
@@ -210,8 +220,7 @@ app.get("/urls/:shortURL",(req, res) => {
 
 //Route to update a URL
 app.post("/urls/:id", (req, res) => {
-  let userid = req.cookies["userid"];
-  console.log("Update_Userid", userid);
+  let userid = req.session.userid;
   let shortURL = req.params.id;
   if (userid === urlDatabase[shortURL].userID) {
     urlDatabase[shortURL] = { longURL: req.body.longURL, userID: userid};
@@ -219,19 +228,11 @@ app.post("/urls/:id", (req, res) => {
   }
 })
 
-//This route renders the tiny URLS page
-// app.get("/u/:shortURL",(req, res) => {
-//   let templateVars = urlDatabase[req.params.shortURL];
-//   res.render("urls_show", templateVars);
-// });
-
-
 //This route renders the tiny URLS page to anyone, even when the user is not logged in.
 app.get("/u/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
   res.render("showShortUrl", templateVars);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app is listening on port ${PORT}`);
